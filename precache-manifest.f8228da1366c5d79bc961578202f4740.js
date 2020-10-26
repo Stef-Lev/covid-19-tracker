600368b421adad29ff795e7f63e85ee20d310148
Stefanos Leventis <sigianas@yahoo.com>
2020-10-27 00:33:53 +0200

    Updates

---
+++ precache-manifest.f8228da1366c5d79bc961578202f4740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098b28d6f82adf2c31b54b00a63e91d9",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a2afaa21e16165c5411c",
+    "url": "/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "427ced923538ee49eaa9",
+    "url": "/covid-19-tracker/static/css/main.d4d50b63.chunk.css"
+  },
+  {
+    "revision": "a2afaa21e16165c5411c",
+    "url": "/covid-19-tracker/static/js/2.ffb59b46.chunk.js"
+  },
+  {
+    "revision": "0b110a718d0a8fc6b16693cbaab585fa",
+    "url": "/covid-19-tracker/static/js/2.ffb59b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427ced923538ee49eaa9",
+    "url": "/covid-19-tracker/static/js/main.c9c039f3.chunk.js"
+  },
+  {
+    "revision": "81a55b44d7d7d1761ae9",
+    "url": "/covid-19-tracker/static/js/runtime-main.c7dd29d7.js"
+  }
+]);
